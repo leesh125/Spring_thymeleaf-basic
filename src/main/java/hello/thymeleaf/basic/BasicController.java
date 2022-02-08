fc3b8089159e8612493683d06e728b3dfa4fb902
leesh125 <chelsea12345@naver.com>
2022-02-09 02:35:56 +0900

    속성 값 설정 - 타임리프는 주로 HTML 태그에 th:* 속성을 지정하는 방식으로 동작한다. th:* 로 속성을 적용하면 기존 속성을 대체한다. 기존 속성이 없으면 새로 만든다

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -82,6 +82,11 @@ public class BasicController {
         return "basic/literal";
     }
 
+    @GetMapping("/attribute")
+    public String attribute() {
+        return "basic/attribute";
+    }
+
     @Component("helloBean")
     static class HelloBean {
         public String hello(String data) {
